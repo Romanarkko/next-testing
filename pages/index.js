732c009f639f079dbd603d129bac753587ad0baa
rkonoshchuk-amplify <rkonoshchuk@amplify.com>
2022-10-14 15:19:54 +0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import MainContainer from '../components/MainContainer';
+
+const Index = () => {
+  return (
+    <MainContainer keywords={'main page'}>
+      <div>
+        <h1>Main Page</h1>
+      </div>
+    </MainContainer>
+  );
+};
+
+export default Index;
